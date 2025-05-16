ba3b5de621d4548ff155be081b9427e7b3b2e3c2
AwuaPoyoyo <akuaporc@gmail.com>
2025-05-16 13:18:30 +0800

    dwd

---
+++ pages/post.js
@@ -0,0 +1,15 @@
+import axios from "axios";
+
+export default function Post() {
+    const [create, setCreate] = useState(false);
+    const [name, SetName] = useState("";
+console.log(name;)
+
+    )
+    const handlesubmit = ()
+  return (
+    <div>
+
+    </div>
+  );
+}
